502e6a2b0e1fc8212c1b86e6e071f8b7ea31ebad
Sergi Vaque <galdons@emea.roche.com>
2021-07-26 16:07:19 +0200

    Refactored getTripByUser()

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,23 +10,15 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = loggedInUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = tripsBy(user);
-			}
-			return tripList;
-		} else {
+		if (loggedInUser() == null) {
 			throw new UserNotLoggedInException();
 		}
+
+		List<Trip> tripList = new ArrayList<Trip>();
+		return user.isFriendsWith(loggedInUser())
+						? tripsBy(user)
+						: new ArrayList<Trip>();
+
 	}
 
 	List<Trip> tripsBy(User user) {
